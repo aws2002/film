f074fdeeec3a39eeb3e94590f4888822d81317ae
Osama Husam <osamahusam848@gmail.com>
2022-08-09 11:52:40 +0300

    eidt

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,15 +1,37 @@
 const request = require("request");
+const fs = require("fs");
+const path = require('path');
+
+const filePath = path.join(__dirname, '../public/js/data.json');
 const displayRandomMovies = (req, res) => {
-    const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1cf50e6248dc270629e802686245c2c8&page=${
-      Math.floor(Math.random() * 100) + 1
-    }`;
-    request(url, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        const data = JSON.parse(body);
-        res.render("index", { data: data.results ,title:"Home"});
-      }
-    });
-  };
+  fs.readFile(filePath, "utf8", (err, jsonString) => {
+    if (err) {
+      console.log("Error reading file from disk:", err);
+      return;
+    }
+    try {
+      const customer = JSON.parse(jsonString);
+      res.render("index", { data: customer.results ,title:"Home"});
+    } catch (err) {
+      console.log("Error parsing JSON string:", err);
+    }
+  });
+};
+
+//? data from API
+
+// const displayRandomMovies = (req, res) => {
+//     const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1cf50e6248dc270629e802686245c2c8&page=${
+//       Math.floor(Math.random() * 100) + 1
+//     }`;
+//     request(url, (error, response, body) => {
+//       if (!error && response.statusCode === 200) {
+//         const data = JSON.parse(body);
+//         res.render("index", { data: data.results ,title:"Home"});
+//       }
+//     });
+//   };
+
   const searchMovie = (req, res) => {
     const movie = req.query.search;
     const url =
